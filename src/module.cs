b9c7509c24019a8bdef108129cd357232d572e15
Evelyn Veilwinter <evelyntsmg@gmail.com>
2024-09-27 23:08:56 -0300

    Initial commit: Code

---
+++ src/module.cs
@@ -0,0 +1,201 @@
+﻿global using System;
+global using Fahrenheit.CoreLib.FFX;
+
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+using System.Text.Json.Serialization;
+using Fahrenheit.CoreLib;
+using Fahrenheit.CoreLib.FFX.Atel;
+
+namespace Fahrenheit.Modules.CSR;
+
+public sealed record CSRModuleConfig : FhModuleConfig {
+    [JsonConstructor]
+    public CSRModuleConfig(string configName, bool configEnabled) : base(configName, configEnabled) { }
+
+    public override bool TrySpawnModule([NotNullWhen(true)] out FhModule? fm) {
+        fm = new CSRModule(this);
+        return fm.ModuleState == FhModuleState.InitSuccess;
+    }
+}
+
+public unsafe class CSRModule : FhModule {
+    private readonly CSRModuleConfig _moduleConfig;
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate void AtelEventSetUp(u32 event_id);
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate char* AtelGetEventName(u32 event_id);
+
+    public static char* get_event_name(u32 event_id)
+        => FhUtil.get_fptr<AtelGetEventName>(0x4796e0)(event_id);
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate void TOMkpCrossExtMesFontLClutTypeRGBA(
+            u32 p1,
+            u8 *text,
+            f32 x, f32 y,
+            u8 color,
+            u8 p6,
+            u8 tint_r, u8 tint_g, u8 tint_b, u8 tint_a,
+            f32 scale,
+            f32 _);
+
+    public static void draw_text_rgba(
+            u32 p1,
+            u8[] text,
+            f32 x, f32 y,
+            u8 color,
+            u8 p6,
+            u8 tint_r, u8 tint_g, u8 tint_b, u8 tint_a,
+            f32 scale,
+            f32 _) {
+        fixed (u8 *_text = text)
+        FhUtil.get_fptr<TOMkpCrossExtMesFontLClutTypeRGBA>(0x501700)(p1, _text, x, y, color, p6, tint_r, tint_g, tint_b, tint_a, scale, _);
+    }
+
+    public static void draw_text(
+            u32 p1,
+            u8[] text,
+            f32 x, f32 y,
+            u8 color,
+            u8 p6,
+            f32 scale,
+            f32 _) {
+        draw_text_rgba(p1, text, x, y, color, p6, 0x80, 0x80, 0x80, 0x80, scale, _);
+    }
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    private delegate u32 FUN_0086e990(isize signal_info_ptr);
+
+    private FhMethodHandle<FUN_0086e990> _work_debug;
+    private FhMethodHandle<AtelEventSetUp> _csr_event;
+
+    public delegate void CsrEvent(u8* code_ptr);
+
+    public static readonly Dictionary<string, CsrEvent> removers = new();
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    internal delegate u32 AtelCallTargetExec(AtelBasicWorker* work, AtelStack* stack);
+
+    public CSRModule(CSRModuleConfig moduleConfig) : base(moduleConfig) {
+        _moduleConfig = moduleConfig;
+
+        _work_debug = new(this, "FFX.exe", work_debug, offset: 0x46e990);
+        _csr_event = new(this, "FFX.exe", csr_event, offset: 0x472e90);
+
+        _moduleState  = FhModuleState.InitSuccess;
+    }
+
+    public override bool FhModuleInit() {
+        Removers.init();
+
+        return _csr_event.hook()
+            && _work_debug.hook();
+    }
+
+    private u32 work_debug(isize signal_info_ptr) {
+        u32 ret = _work_debug.orig_fptr(signal_info_ptr);
+
+        add_work(*(i16*)(signal_info_ptr + 0x4), *(i16*)(signal_info_ptr + 0x10));
+
+        return ret;
+    }
+
+    private static void add_work(i16 work_id, i16 entry_id) {
+        // Was it already executed recently?
+        for (i32 i = 0; i < rew.Count; i++) {
+            var o = rew[i];
+            if (o.work_id == work_id) {
+                o.entry_id = entry_id;
+                o.last_updated = 0;
+                return;
+            }
+        }
+
+        // Can we just add?
+        if (rew.Count < 40) {
+            rew.Add(new DisplayObject { work_id = work_id, entry_id = entry_id, last_updated = 0 });
+            return;
+        }
+
+        // Find oldest work to replace
+        i32 max = rew[0].last_updated;
+        i32 longest_idx = 0;
+        for (i32 i = 0; i < rew.Count; i++) {
+            var o = rew[i];
+            if (o.last_updated > max) {
+                max = o.last_updated;
+                longest_idx = i;
+            }
+        }
+
+        rew[longest_idx] = new DisplayObject { work_id = work_id, entry_id = entry_id, last_updated = 0 };
+    }
+
+    private struct DisplayObject {
+        public i16 work_id;
+        public i16 entry_id;
+        public i32 last_updated;
+    }
+
+    private static List<DisplayObject> rew = new(40);
+    public override void render_game() {
+        draw_text(0, FhCharset.Us.to_bytes("CSR is running!"), x: 430, y: 5, color: 0x00, 0, scale: 0.5f, 0);
+
+        string event_name = Marshal.PtrToStringAnsi((isize)get_event_name(*(u32*)Globals.event_id))!;
+        draw_text(0, FhCharset.Us.to_bytes($"Event: {event_name}"), x: 430, y: 15, color: 0x00, 0, scale: 0.5f, 0);
+
+        List<u8> works = new();
+        byte[] head = FhCharset.Us.to_bytes("Recent Signal Targets:\n");
+
+        for (i32 i = 0; i < head.Length; i++) {
+            works.Add(head[i]);
+        }
+
+        const u64 colors = 0x7040603050;
+        for (i32 i = 0; i < rew.Count; i++) {
+            var o = rew[i];
+            if (o.last_updated < 10) {
+                works.Add(0xA);
+                works.Add((u8)((colors >> (8 * (o.last_updated >> 1))) & 0xFF));
+            }
+
+            byte[] work_id = FhCharset.Us.to_bytes($"{o.work_id:X4}:{o.entry_id:X4}, ");
+            for (i32 j = 0; j < 10; j++) {
+                works.Add(work_id[j]);
+            }
+
+            works.Add(0xA);
+            works.Add(0x0);
+
+            if (i % 2 == 1) works.Add(0x2);
+        }
+
+        draw_text(0, works.ToArray(), x: 430, y: 25, color: 0x00, 0, scale: 0.5f, 0);
+    }
+
+    public override void post_update() {
+        for (i32 i = 0; i < rew.Count; i++) {
+            var o = rew[i];
+            o.last_updated++;
+        }
+    }
+
+    public void csr_event(u32 event_id) {
+        _csr_event.orig_fptr(event_id);
+
+        rew.Clear();
+
+        string event_name = Marshal.PtrToStringAnsi((isize)get_event_name(event_id))!;
+        if (removers.TryGetValue(event_name, out CsrEvent? remover)) {
+            FhLog.Info($"Remover available for event \"{event_name}\"");
+            u8* code_ptr = Globals.Atel.controllers[0].worker(0)->code_ptr;
+            remover(code_ptr);
+        } else {
+            FhLog.Info($"Remover not available for event \"{event_name}\"");
+        }
+    }
+}
---
+++ src/removers.cs
@@ -0,0 +1,68 @@
+﻿using Fahrenheit.CoreLib.FFX.Atel;
+using System.Runtime.InteropServices;
+
+using static Fahrenheit.Modules.CSR.CSRModule;
+
+namespace Fahrenheit.Modules.CSR;
+
+internal static unsafe partial class Removers {
+    [LibraryImport("msvcrt.dll", EntryPoint = "memset", SetLastError = false)]
+    [UnmanagedCallConv(CallConvs = new[] { typeof(System.Runtime.CompilerServices.CallConvCdecl) })]
+    private static partial isize memset(isize dst, i32 value, isize size);
+
+    public static void init() {
+        removers.Add("bsil0300", remove_besaid_valley);
+        removers.Add("bsil0600", remove_besaid_promontory);
+        removers.Add("bsil0700", remove_besaid_village_slope);
+    }
+
+    private static void remove(u8* code_ptr, i32 from, i32 to) {
+        memset((isize)code_ptr + from, 0, to - from);
+    }
+
+    private static void set(u8* code_ptr, i32 offset, u8 value) {
+        *(code_ptr + offset) = value;
+    }
+
+    private static void set(u8* code_ptr, i32 offset, AtelOpCode opcode) {
+        u8* ptr = code_ptr + offset;
+        foreach (u8 b in opcode.to_bytes()) {
+            *ptr = b;
+            ptr++;
+        }
+    }
+
+    private static void set_tp(u8* code_ptr, i32 offset, u16 x_idx, u16 y_idx, u16 z_idx) {
+        u8* ptr = code_ptr + offset;
+        set(ptr, 0x0, AtelInst.PUSHF.build(x_idx));
+        set(ptr, 0x3, AtelInst.PUSHF.build(y_idx));
+        set(ptr, 0x6, AtelInst.PUSHF.build(z_idx));
+        set(ptr, 0x9, AtelInst.CALLPOPA.build(0x126));
+    }
+
+    internal static void remove_besaid_valley(u8* code_ptr) {
+        remove(code_ptr, 0x1B29, 0x1C43); // Wakka pushes Tidus into the water
+
+        remove(code_ptr, 0x3398, 0x33C6); // Initial fadeout into cutscene
+        remove(code_ptr, 0x1CFC, 0x1FCF); // Wakka asks Tidus to join the Aurochs
+
+        // Skip the Promontory since it'd be instantly skipped anyway
+        // We essentially copy bsil0600:3EB3..3EC8 to bsil0300:1FCF..1FEA
+        set(code_ptr, 0x1FCF, AtelInst.PUSHII.build(0x7E)); // GameMoment = 124 -> GameMoment = 126
+        set(code_ptr, 0x1FD5, AtelInst.PUSHII.build(0x0F)); // Common.00BB(0) -> Common.00BB(15)
+        set(code_ptr, 0x1FE1, AtelInst.PUSHII.build(69));
+        set(code_ptr, 0x1FE7, AtelInst.CALL.build(0x11)); // Common.010C(67, 0) -> Common.transitionToMap(69, 0)
+    }
+
+    internal static void remove_besaid_promontory(u8* code_ptr) {
+        remove(code_ptr, 0x3DB4, 0x3EAD); // Cutscene coming from Valley DEPRECATED
+    }
+
+    internal static void remove_besaid_village_slope(u8* code_ptr) {
+        remove(code_ptr, 0x264D, 0x289A); // First cutscene
+
+        remove(code_ptr, 0x28B7, 0x28C0); // Don't make the game wait for a fade that won't happen
+
+        set_tp(code_ptr, 0x264D, 0xD, 0xE, 0xF); // Set player position to the vanilla post-cutscene one
+    }
+}
